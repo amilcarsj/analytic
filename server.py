46bbab69cd8ac024a1db1054209e71ff1a507996
amilcarsj <amilcarsj@gmail.com>
2016-11-01 18:28:21 -0300

    fixes bug with ports

--- server.py
+++ server.py
@@ -84,4 +84,4 @@ def classify():
     print json.dumps(trajectories_to_label)
     return json.dumps(trajectories_to_label)
 
-run(host='0.0.0.0', port=8080, debug=True)
+run(host='0.0.0.0', port=80, debug=True)
